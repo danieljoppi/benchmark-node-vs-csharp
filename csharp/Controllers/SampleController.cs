7d8e7b497294bc01098343fe118dd86392b2131e
Guilherme Bauer <Guilherme Bauer>
2016-02-02 22:02:34 -0200

    Added a sample controller with json serialization

---
+++ csharp/Controllers/SampleController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace csharp.Controllers
+{
+    [Route("/api/[controller]")]
+    public class SampleController : Controller
+    {
+        // Sample uri http://localhost:5000/api/sample/1/2
+        [HttpGet("{value1}/{value2}")]
+        public IActionResult SampleGet(int value1, int value2)
+        {
+            return Json(new { result = value1 * value2 });
+        }
+
+        // Sample uri http://localhost:5000/api body 
+        [HttpPost]
+        public IActionResult SamplePostWithDto([FromBody]RequestSampleDto body)
+        {
+            if (!ModelState.IsValid)
+                return HttpBadRequest(ModelState);
+
+            var response = new ResponseSampleDto
+            {
+                Result = body.Value1 * body.Value2
+            };
+
+            return Json(response);
+        }
+
+        [HttpPost("dynamic")]
+        public IActionResult SamplePostWithDynamic([FromBody]dynamic body)
+        {
+            if (!ModelState.IsValid)
+                return HttpBadRequest(ModelState);
+
+            int value1 = 0;
+            int value2 = 0;
+
+            if (int.TryParse(body.value1.ToString(), out value1) &&
+                int.TryParse(body.value2.ToString(), out value2))
+                return Json(new { result = value1 * value2 });
+
+            return HttpBadRequest();
+        }
+    }
+
+    public class RequestSampleDto
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+    }
+
+    public class ResponseSampleDto
+    {
+        public int Result { get; set; }
+    }
+}
--- csharp/Startup.cs
+++ csharp/Startup.cs
@@ -6,26 +6,32 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Serialization;
 
 namespace EmptyApplication
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore().AddJsonFormatters(a => a.ContractResolver = new CamelCasePropertyNamesContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
+            app.UseMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
 
         // Entry point for the application.
